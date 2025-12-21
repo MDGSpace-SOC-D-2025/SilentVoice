5999c49e35d0dacfc07cb947240830f859af7d52
Kasak <kasak3752@gmail.com>
2025-12-21 13:02:48 +0530

    Add calculator button grid layout

--- silentvoice/lib/screens/calculator_screen.dart
+++ silentvoice/lib/screens/calculator_screen.dart
@@ -5,6 +5,73 @@ class CalculatorScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Calculator Screen')));
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 🔹 DISPLAY AREA
+            Expanded(
+              flex: 4,
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: const [
+                    Text("5 - 9"),
+                    SizedBox(height: 8),
+                    Text("-4", style: TextStyle(fontSize: 40)),
+                  ],
+                ),
+              ),
+            ),
+
+            // 🔹 BUTTON AREA (empty placeholder for now)
+            Expanded(
+              flex: 7,
+              child: Container(
+                width: double.infinity,
+                color: const Color.fromARGB(255, 231, 229, 229),
+                padding: const EdgeInsets.all(12),
+                child: GridView.count(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                  children: const [
+                    Text("⌫"),
+                    Text("AC"),
+                    Text("+"),
+                    Text("-"),
+
+                    Text("7"),
+                    Text("8"),
+                    Text("9"),
+                    Text("×"),
+
+                    Text("4"),
+                    Text("5"),
+                    Text("6"),
+                    Text("÷"),
+
+                    Text("1"),
+                    Text("2"),
+                    Text("3"),
+                    Text(""),
+
+                    Text("±"),
+                    Text("0"),
+                    Text("."),
+                    Text("="),
+                  ],
+
+                  // buttons will go here
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
