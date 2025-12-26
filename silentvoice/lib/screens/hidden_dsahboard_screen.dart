4ae8fce50ab058ee07a5e6dbd1cd756b11b1570c
Kasak <kasak3752@gmail.com>
2025-12-26 23:52:43 +0530

    feat: add user hidden dashboard screen

---
+++ silentvoice/lib/screens/hidden_dsahboard_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'calculator_screen.dart';
+import 'know_your_rights_main.dart';
+
+class HiddenDashboardScreen extends StatelessWidget {
+  const HiddenDashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  const Spacer(),
+
+                  IconButton(
+                    icon: const Icon(Icons.settings, size: 32),
+                    onPressed: () {},
+                  ),
+                  SizedBox(width: 2),
+
+                  IconButton(
+                    icon: const Icon(Icons.calculate_outlined, size: 32),
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => const CalculatorScreen(),
+                        ),
+                        (route) => false,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 45),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                children: [
+                  DashboardPillButton(
+                    title: 'Anomynous Chat',
+                    gradient: const [Color(0xFF5C9DFF), Color(0xFF8BB8FF)],
+                    icon: Icons.chat_bubble_outline,
+                    onTap: () {},
+                  ),
+                  const SizedBox(height: 24),
+                  DashboardPillButton(
+                    title: 'Emergency SOS',
+                    gradient: const [Color(0xFFFFB347), Color(0xFFFFCC80)],
+                    icon: Icons.warning_amber_rounded,
+                    onTap: () {},
+                  ),
+                  const SizedBox(height: 24),
+                  DashboardPillButton(
+                    title: ' Evidence Vault',
+                    gradient: const [Color(0xFF9C7CFF), Color(0xFFC3B1FF)],
+                    icon: Icons.lock_outline,
+                    onTap: () {},
+                  ),
+                  const SizedBox(height: 24),
+
+                  DashboardPillButton(
+                    title: 'Nearby Help Map',
+                    icon: Icons.location_on_outlined,
+                    gradient: const [Color(0xFF5FD38D), Color(0xFF9BE7B2)],
+                    onTap: () {},
+                  ),
+                  const SizedBox(height: 24),
+
+                  DashboardPillButton(
+                    title: 'Know Your Rights',
+                    icon: Icons.menu_book_outlined,
+                    gradient: const [Color(0xFF4DB6AC), Color(0xFF80DEEA)],
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const KnowYourRightsHome(),
+                        ),
+                      );
+                    },
+                  ),
+                  const SizedBox(height: 24),
+                  DashboardPillButton(
+                    title: 'Incoming Call',
+                    icon: Icons.call,
+                    gradient: const [Color(0xFF607D8B), Color(0xFF90A4AE)],
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardPillButton extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final List<Color> gradient;
+  final VoidCallback onTap;
+
+  const DashboardPillButton({
+    super.key,
+    required this.title,
+    required this.gradient,
+    required this.icon,
+    required this.onTap,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(40),
+      onTap: onTap,
+      child: Container(
+        height: 86,
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(40),
+          gradient: LinearGradient(
+            colors: gradient,
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Row(
+          children: [
+            Icon(icon, color: Colors.white, size: 30),
+            const SizedBox(width: 14),
+            Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
