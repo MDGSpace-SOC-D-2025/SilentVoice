6f444fae00efc291ed9fe1eb10c6ae6113b03c3e
Kasak <kasak3752@gmail.com>
2026-01-01 23:36:25 +0530

    feat: integrate Firebase with anonymous and helper auth

---
+++ silentvoice/lib/auth/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<User?> signInAnonymously() async {
+    try {
+      final result = await _auth.signInAnonymously();
+      return result.user;
+    } catch (e) {
+      print('Anonymous login failed: $e');
+      return null;
+    }
+  }
+
+  Future<User?> signInHelperWithEmail(String email, String password) async {
+    try {
+      final result = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return result.user;
+    } catch (e) {
+      print('Helper login failed: $e');
+      return null;
+    }
+  }
+
+  User? get currentUser => _auth.currentUser;
+}
--- silentvoice/lib/main.dart
+++ silentvoice/lib/main.dart
@@ -1,7 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:silentvoice/firebase_options.dart';
 import 'screens/calculator_screen.dart';
+import 'package:silentvoice/auth/auth_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  final AuthService authService = AuthService();
+
+  final user = await authService.signInAnonymously();
+
+  if (user == null) {
+    debugPrint("Anonymous login failed");
+  }
+
   runApp(const MyApp());
 }
 
